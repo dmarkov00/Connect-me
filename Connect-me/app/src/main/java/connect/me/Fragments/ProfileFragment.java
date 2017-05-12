89ac163f1926095e300b29184e743cfc1f92c2e5
Dimitar Markov <dmarkov23@gmail.com>
2017-05-12 17:15:43 +0200

    finalizing desing of profile page

--- Connect-me/app/src/main/java/connect/me/Fragments/ProfileFragment.java
+++ Connect-me/app/src/main/java/connect/me/Fragments/ProfileFragment.java
@@ -41,7 +41,7 @@ public class ProfileFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.material_design_profile_screen_xml_ui_design, container, false);
     }
 
     @Override
