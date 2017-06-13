a771d69aa2ba0a3fc64858d929b6873f9a80162b
Salvador,Alexander A <a.salvador@student.fontys.nl>
2017-06-13 16:24:31 +0200

    profile page and data updated

--- Connect-me/app/src/main/java/connect/me/Fragments/ProfileFragment.java
+++ Connect-me/app/src/main/java/connect/me/Fragments/ProfileFragment.java
@@ -4,10 +4,12 @@ package connect.me.fragments;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.DialogFragment;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -45,7 +47,8 @@ public class ProfileFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
     }
 
     @Override
@@ -57,9 +60,13 @@ public class ProfileFragment extends DialogFragment {
 
         TextView nameTextView = (TextView) view.findViewById(R.id.user_profile_name);
         TextView phoneTextView = (TextView) view.findViewById(R.id.user_profile_phone_number);
+        TextView genderTextView = (TextView) view.findViewById(R.id.user_profile_gender);
+        TextView ageTextView = (TextView) view.findViewById(R.id.user_profile_age);
 
-//        nameTextView.setText(user.name);
-//        phoneTextView.setText(user.phone);
+        nameTextView.setText(user.getName());
+        phoneTextView.setText("Phone: " + user.getPhoneNumber());
+        genderTextView.setText("Sex: " + user.getGender());
+        ageTextView.setText("Age: "+ user.getAge()+"");
 
     }
 
