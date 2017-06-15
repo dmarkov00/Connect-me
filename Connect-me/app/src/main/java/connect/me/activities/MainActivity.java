1f75c4c66a2bbb0ccdbbf8790072f042d4e4a314
annavesnenok <anna.vesnenok@gmail.com>
2017-06-15 14:14:36 +0200

    Fixed version for displaying markers. Phone field fixed in the additionalUserData activity

--- Connect-me/app/src/main/java/connect/me/activities/MainActivity.java
+++ Connect-me/app/src/main/java/connect/me/activities/MainActivity.java
@@ -200,6 +200,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mLocationRequest = LocationRequest.create();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); //gives the precise location of the user
         mLocationRequest.setInterval(1000); //every second the users location is refreshed
+        mLocationRequest.setSmallestDisplacement(5);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -214,6 +215,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         }
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this); //gets the user location depending on the mLocationRequest object
+
 
     }
 
@@ -245,14 +247,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     public void placeMarker(LatLng ll, String key) {
-        if (firebaseAuth.getCurrentUser().getUid().equals(key)) {
-
-            for (Marker m : markersList) {
-                if (m.getTitle().equals(key)) {
-                    m.remove(); //removes the previous current location
-                }
-            }
-        }
+//        if (firebaseAuth.getCurrentUser().getUid().equals(key)) {
+//
+//            for (Marker m : markersList) {
+//                if (m.getTitle().equals(key)) {
+//                    m.remove(); //removes the previous current location
+//                }
+//            }
+//        }
         MarkerOptions options;
 
 
@@ -265,12 +267,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
 
-        for (Marker m : markersList) {
-            if (m.getTitle().equals(options.getTitle())) {
-                markersList.remove(m);
-                break;
-            }
-        }
+//        for (Marker m : markersList) {
+//            if (m.getTitle().equals(options.getTitle())) {
+//                markersList.remove(m);
+//                break;
+//            }
+//        }
 
 
         myMarker = mGoogleMap.addMarker(options);
@@ -303,6 +305,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 boolean vibration = false;
+                mGoogleMap.clear();
                 for (DataSnapshot u : dataSnapshot.getChildren()) {
 
                     Location locationOfOthers = Helpers.convertToLocation(u.child("longitude").getValue(Double.class), u.child("latitude").getValue(Double.class));
